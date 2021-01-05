eae6ef0bc0042d8e2a31e6e7cd71b1a2fb5eedaf
Obote <obotedenis256@gmail.com>
2021-01-05 14:51:16 +0300

    Validating the do list

--- todos/app.js
+++ todos/app.js
@@ -15,5 +15,9 @@ const generateTemplate = todo =>{
 addForm.addEventListener('submit', e => {
     e.preventDefault(); 
     const todo = addForm.add.value.trim();
-  generateTemplate(todo);
+
+    if(todo.length){
+        generateTemplate(todo);
+    }
+ 
 });
